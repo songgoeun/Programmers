e35280189d7db0a23f3248f019d0ada3221519eb
wave2 <35678491+songgoeun@users.noreply.github.com>
2024-02-25 14:55:10 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.60 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+    let stk = [];
+    
+    for(let i=0; i<arr.length; i) {
+        if(stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+            continue;
+        }
+        
+        if(stk[stk.length-1] < arr[i]) {
+            stk.push(arr[i]);
+            i++;
+            continue;
+        }
+        
+        if(stk[stk.length-1] >= arr[i]) {
+            stk.pop();
+        }
+    }
+    
+    return stk;
+}
