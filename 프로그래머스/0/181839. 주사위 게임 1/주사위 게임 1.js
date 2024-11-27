0d24e64f9e2e0aa3c72ba0ae6143ed5bd6a63247
wave2 <35678491+songgoeun@users.noreply.github.com>
2024-11-27 22:17:30 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    if(a % 2 === 1 && b % 2=== 1) {
+        return Math.pow(a,2) + Math.pow(b,2);
+    }
+    
+    if(a % 2 === 1 || b % 2=== 1) {
+        return 2 * (a + b);
+    }
+    
+    if(a % 2 === 0 && b % 2 === 0) {
+        return Math.abs((a - b));
+    }
+    
+}
